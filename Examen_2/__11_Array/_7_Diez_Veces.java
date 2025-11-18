ea1304678e4998d050cf74b68cc2c676b40422f2
mfl <re-imagine.studio@proton.me>
2025-11-18 15:47:20 +0100

    update 2

--- Examen_2/__11_Array/_7_Diez_Veces.java
+++ Examen_2/__11_Array/_7_Diez_Veces.java
@@ -4,7 +4,11 @@ public class _7_Diez_Veces {
 
 	public static void main(String[] args) {
 
-		
+		/*
+		 * Crea un programa que cree un array de enteros e introduzca la siguiente secuencia de valores: 
+		 * 
+		 * 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, etc. hasta introducir 10 diez veces, y luego la muestre por pantalla.
+		 */
 	}
 
 }
